6a5128f41f26a3cc7b30f57767684df9120f45bf
antoniodias26 <antonio.dias26@escola.pr.gov.br>
2025-06-05 10:58:46 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,13 +1,1 @@
-**Bem vindo ao meu perfil**
-
-Meu nome é Antonio Dias
-
-Sou aluno do componente curricular de pensamento computacional da rede estadual de educação do paraná
-
-Utilizo em aula a plataforma  [Alura](httpss//www.alura.com.br)
-
-###você pode entrar comigo:
-
-antonio.dias26@escola.pr.gov.br
-
-![](https://media1.tenor.com/m/8aR2e6H7yQgAAAAd/asas.gif)
+# flashcard
---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'História',
+    'Quando foi a primeira guerra mundial',
+    '1914'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
